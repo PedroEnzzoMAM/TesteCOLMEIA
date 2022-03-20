dca01549f916ee357fb2362eab71040e92da1878
Enzzo <pedroenzzo123@gmail.com>
2022-03-20 20:25:11 -0300

    TEST API

---
+++ main.js
@@ -0,0 +1,50 @@
+const url = "https://jsonplaceholder.typicode.com/todos"
+
+fetch(url)
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+fetch(`${url}/1`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        id: 1,
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+
+fetch(`${url}/1`, {
+  method: 'PATCH',
+  body: JSON.stringify({
+    title: 'foo',
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+fetch(`${url}/1`, {
+    method: 'DELETE',
+  });
